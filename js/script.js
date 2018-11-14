bfeeb29bad1fd3f16e967bc9b84d9f301498e251
Alex Huynh <alexh3925@gmail.com>
2018-11-14 02:16:37 +0000

    added questions, choices, and css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 /* global $ */
 
-$(document).ready(function() {
-    $("button").click(function() {
+$(document).ready(function(){
+    $("button").click(function(){
         var name = $("#name").val();
-        var techPlacement = "TBD";
-        var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
         var totalScore;
     });
 
